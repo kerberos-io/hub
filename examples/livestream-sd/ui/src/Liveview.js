c2886c55083e089d148c3c3259dd13a95d210bb1
Cedric Verstraeten <cedric@verstraeten.io>
2024-06-14 08:22:26 +0200

    add kerberos-io image card

--- examples/livestream-sd/ui/src/Liveview.js
+++ examples/livestream-sd/ui/src/Liveview.js
@@ -1,13 +1,18 @@
 // This component will receive an array of livestreams and display them in a video grid.
-
 import React from 'react';
+import { ImageCard } from '@kerberos-io/ui';
 
 const Liveview = ({ liveviews }) => {
     return (
         <div className="liveview">
-            {liveviews && liveviews.length && "ok"}
         {liveviews && liveviews.map((liveview, index) => (
-            <img key={index} src={`data:image/jpeg;base64,${liveview.image}`} alt="liveview" />
+            <div>
+                <p>Camera name: {liveview.camera_id}</p>
+                <ImageCard
+                    imageSrc={`data:image/png;base64, ${liveview.image}`}
+                    onerror=""
+                />
+            </div>
         ))}
         </div>
     );
